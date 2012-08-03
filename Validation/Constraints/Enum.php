2422772989e4435335ad8b726ba9c58ba276c5e5
Florian Eckerstorfer <florian@theroadtojoy.at>
2012-08-03 13:35:06 +0200

    Added Enum constriant and validator

---
+++ Validation/Constraints/Enum.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Braincrafted\ValidationBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\MissingOptionsException;
+
+/**
+ * @Annotation
+ */
+class Enum extends Constraint
+{
+    public $message = 'This value must be one of {{ enums }}.';
+    public $enums;
+
+    public function __construct($options = null)
+    {
+        if (null !== $options && !is_array($options)) {
+            $options = array(
+                'enums'    => $options
+            );
+        }
+
+        parent::__construct($options);
+
+        if (null === $this->enums) {
+            throw new MissingOptionsException('The option "enums" must be given for constraint ' . __CLASS__, array('enums'));
+        }
+    }
+
+    public function validatedBy()
+{
+    return get_class($this).'Validator';
+}
+}
---
+++ Validation/Constraints/EnumValidator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Braincrafted\ValidationBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class EnumValidator extends ConstraintValidator
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_scalar($value) && !(is_object($value) && method_exists($value, '__toString'))) {
+            throw new UnexpectedTypeException($value, 'string');
+        }
+
+        $stringValue = (string) $value;
+
+        if (!in_array($stringValue, $constraint->enums)) {
+            $this->context->addViolation($constraint->message, array(
+                '{{ value }}'   => $stringValue,
+                '{{ enums }}'   => implode(', ', $constraint->enums)
+            ), $value, $constraint->enums);
+        }
+    }
+}
