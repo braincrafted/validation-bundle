894e805928405ead64741eaf0b1a731a0841f9d6
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-11-17 23:46:29 +0100

    Fix CS

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,7 +22,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('braincrafted_validation');
+        $treeBuilder->root('braincrafted_validation');
 
         return $treeBuilder;
     }
